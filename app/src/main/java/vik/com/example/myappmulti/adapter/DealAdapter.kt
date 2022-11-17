036beaa6e13d8730a94cd37dfe0113f0f62f74ea
Yilisav <vik3000@list.ru>
2022-11-17 15:13:31 +0500

    RecyclerView without View

--- app/src/main/java/vik/com/example/myappmulti/adapter/DealAdapter.kt
+++ app/src/main/java/vik/com/example/myappmulti/adapter/DealAdapter.kt
@@ -1,13 +1,8 @@
 package vik.com.example.myappmulti.adapter
 
-
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import vik.com.example.myappmulti.R
-import vik.com.example.myappmulti.databinding.FragmentJobListNavLayoutBinding
 import vik.com.example.myappmulti.databinding.ItemDealLayoutBinding
 import vik.com.example.myappmulti.model.DealModel
 
@@ -22,34 +17,31 @@ class DealAdapter
  * @param onItemClickListener - обработчик кликов на элементы списка.                                   *
  * @param onItemRemoveListener - обработчик кликов на кнопку "удалить" элементов списка.                *
  *******************************************************************************************************/
-//    : RecyclerView.Adapter<DealAdapter.DealViewHolder>
     (
-//        private val items                       : MutableList<DealModel>,
+        private val items                       : MutableList<DealModel>,
 //        private val onItemClickListener         : (Int, CObject) -> Unit,
 //        private val onItemRemoveListener        : (Int, CObject) -> Unit
     ) : RecyclerView.Adapter<DealAdapter.DealViewHolder>()
     {
-    private var dealList                      = emptyList<DealModel>()
     /****************************************************************************************************
      * Вспомогательный класс, оотвечающий за визуальное отображение одного элемента данных.             *
      ***************************************************************************************************/
     inner class DealViewHolder
     /****************************************************************************************************
      * Конструктор.                                                                                     *
-     * @param binding - объект, хранящий ссылки на элементы интерфейса, у которых указан идентификатор. *
+     * @param binding - объект, хранящий ссылки на элементы интерфейса с одним элемонтом списка,        *
+     * у которых указан идентификатор. *
      * @param onItemClickListener - обработчик кликов на элемент списка.                                *
      * @param onItemRemoveListener - обработчик кликов на кнопку "удалить" элемента списка.             *
      ***************************************************************************************************/
         (
-//        private val binding                 : FragmentJobListNavLayoutBinding,
+        private  val binding                  : ItemDealLayoutBinding
 //        private val onItemClickListener     : (Int, DealModel) -> Unit,
 //        private val onItemRemoveListener    : (Int, DealModel) -> Unit
-        view: View
-        )                                   : RecyclerView.ViewHolder(view)
-//        )                                   : RecyclerView.ViewHolder(binding.root)
+        )                                     : RecyclerView.ViewHolder(binding.root)
     {
-//        Элемент данных, который отображается в текущем элементе списка.
-//        private lateinit var item           : DealModel
+       /**Элемент данных, который отображается в текущем элементе списка.*/
+        private lateinit var item             : DealModel
 
 //        init{
 //            //Обработка клика на все поля элемента, кроме кнопки с корзиной.
@@ -70,21 +62,15 @@ class DealAdapter
          * Метод описывает логику вывода элемента данных в строку списка.                               *
          * @param newItem - элемент данных для вывода.                                                  *
          ***********************************************************************************************/
-//        fun bind(
-//            newItem                             : DealModel
-//        )
-//        {
-//            item                                = newItem
-//            val firstName : TextView = binding.rvDeal.findViewById(R.id.tv_first_name_holder)
-//            val lastName : TextView = binding.rvDeal.findViewById(R.id.tv_last_name_holder)
-//            val idService : TextView = binding.rvDeal.findViewById(R.id.tv_id_service_holder)
-////            binding.tvFirstNameHolder.text  = newItem.clientFirstName
-////            binding.tvLastNameHolder.text = newItem.clientLastName
-////            binding.tvIdServiceHolder.text = newItem.idService.toString()
-//            firstName.text = newItem.clientFirstName
-//            lastName.text = newItem.clientLastName
-//            idService.text  = newItem.idService.toString()
-//        }
+        fun bind(
+            newItem                             : DealModel
+        )
+        {
+            item                                = newItem
+            binding.tvFirstNameHolder.text      = newItem.clientFirstName
+            binding.tvLastNameHolder.text       = newItem.clientLastName
+            binding.tvIdServiceHolder.text      = newItem.idService.toString()
+        }
     }
 
     /****************************************************************************************************
@@ -92,16 +78,11 @@ class DealAdapter
      * Указывает, какой файл с разметкой внешнего вида использовать.                                    *
      * @param parent - ссылка на родительский элемент - RecyclerView.                                   *
      ***************************************************************************************************/
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DealViewHolder {
-//       val  binding                             = FragmentJobListNavLayoutBinding.inflate(
-//           LayoutInflater.from(parent.context),parent,false)
-//       return DealViewHolder(
-//           binding,
-   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DealViewHolder {
-       val  view = LayoutInflater.from(parent.context).inflate(R.layout.item_deal_layout,parent,false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DealViewHolder {
+       val  binding                             = ItemDealLayoutBinding.inflate(
+           LayoutInflater.from(parent.context),parent,false)
        return DealViewHolder(
-
-           view,
+           binding,
 //           onItemClickListener,
 //           onItemRemoveListener
        )
@@ -113,27 +94,20 @@ class DealAdapter
      * @param position - порядковый номер элемента данных в списке.                                     *
      ***************************************************************************************************/
     override fun onBindViewHolder(holder: DealViewHolder, position: Int) {
-//        holder.bind(items[position])
-        val firstName : TextView = holder.itemView.findViewById(R.id.tv_first_name_holder)
-        val lastName : TextView = holder.itemView.findViewById(R.id.tv_last_name_holder)
-        val idService : TextView = holder.itemView.findViewById(R.id.tv_id_service_holder)
-        firstName.text = dealList[position].clientFirstName
-        lastName.text = dealList[position].clientLastName
-        idService.text = dealList[position].idService.toString()
-
+        holder.bind(items[position])
     }
     /****************************************************************************************************
      * Возвращает актуальное количество элементов в списке.                                             *
      * @return общее количество элементов данных в списке.                                              *
      ***************************************************************************************************/
     override fun getItemCount(): Int {
-//        return items.size
-        return dealList.size
+        return items.size
+
     }
-    fun setList(list: ArrayList<DealModel>){
-        dealList = list
-        notifyDataSetChanged()
-    }
+//    fun setList(list: ArrayList<DealModel>){
+//        dealList = list
+//        notifyDataSetChanged()
+//    }
 
 //    interface IItemClickListener{
 //        fun onItemClick(position: Int, userList: DealModel)
--- app/src/main/java/vik/com/example/myappmulti/screens/CJobListNavigator.kt
+++ app/src/main/java/vik/com/example/myappmulti/screens/CJobListNavigator.kt
@@ -1,6 +1,5 @@
 package vik.com.example.myappmulti.screens
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,12 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import vik.com.example.myappmulti.MAIN
-import vik.com.example.myappmulti.R
-import vik.com.example.myappmulti.activities.CJobInfo
 import vik.com.example.myappmulti.adapter.DealAdapter
 import vik.com.example.myappmulti.databinding.FragmentJobListNavLayoutBinding
 import vik.com.example.myappmulti.model.DealModel
@@ -21,20 +15,18 @@ import vik.com.example.myappmulti.model.DealModel
 
 class CJobListNavigator : Fragment() //, View.OnClickListener
     {
-
-    //Объект класса, содержащий ссылки на управляющие графические элементы интерфейса пользователя.
+    /** @param binding - Объект класса, содержащий ссылки на управляющие графические элементы интерфейса пользователя.*/
     private lateinit var binding            : FragmentJobListNavLayoutBinding
     private lateinit var resultLauncherInfo : ActivityResultLauncher<Intent>
-    lateinit var adapter                    : DealAdapter
     lateinit var recyclerView               : RecyclerView
 
 
-    //Вызывается для создания компонентов внутри фрагмента
+    // Функция вызывается для создания компонентов внутри фрагмента
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        //Связываем код актиности с файлом, описывающим внешний вид активности.
+        //Связываем код активности с файлом, описывающим макет(вид) активности.
         binding = FragmentJobListNavLayoutBinding.inflate(layoutInflater, container, false )
         return binding.root
     }
@@ -42,39 +34,29 @@ class CJobListNavigator : Fragment() //, View.OnClickListener
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view,savedInstanceState)
 
-//        resultLauncherInfo = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-//            if (result.resultCode == Activity.RESULT_OK) {
-//                val data: Intent? = result.data
-//            }
-//        }
-        initial()
+        /**Тестовый список объектов, которые будут выводится пользователю.*/
+        val items = mutableListOf<DealModel>()
+        items.add(DealModel(123,"новая", "Petrov", "Vasya", "Принтер", "Ремонт", "не включается", "2022-09-11","","",""))
+        items.add(DealModel(135,"новая", "Arhipov", "Artem", "Компьютер", "Ремонт", "не включается", "2022-01-22","","",""))
+        items.add(DealModel(295,"новая", "Sidirov", "Jenya", "Монитор", "Ремонт", "не включается", "2022-11-11","","",""))
+        items.add(DealModel(335,"в работе", "Ivanov", "Ivan", "Принтер", "Ремонт", "не включается", "2022-10-02","","Komkov","Alex"))
+        items.add(DealModel(113,"новая", "Afonin", "Vasya", "Монитор", "Ремонт", "не включается", "2022-04-05","","",""))
+        items.add(DealModel(266,"новая", "Suvorov", "Petr", "Принтер", "Настройка", "не печатает", "2022-09-30","","",""))
+        items.add(DealModel(359,"закрыта", "Suvorov", "Vova", "Компьютер", "Установка ОС", "не загружается", "2022-05-10","2022-06-11","Ivanov","Petr"))
+        items.add(DealModel(135,"в работе", "Ivanov", "Vasya", "Принтер", "Картридж", "кончилась краска", "2022-10-02","","Komkov","Alex"))
+        items.add(DealModel(291,"в работе", "Lenin", "Yan", "Компьютер", "Вирусы", "заблокирован", "2022-10-02","","Ivanov","Alex"))
+        items.add(DealModel(385,"в работе", "Newton", "Isak", "Монитор", "Настройка", "изображение розовое", "2022-10-02","","Ivanov","Alex"))
+        items.add(DealModel(523,"закрыта", "Petrov", "Vasya", "Принтер", "Ремонт", "не печтатает", "2022-11-13","2022-11-16","Komkov","Alex"))
+        items.add(DealModel(216,"новая", "Isaev", "Kirill", "Компьютер", "Настройка", "нет второго диска", "2022-09-30","","",""))
+        items.add(DealModel(381,"в работе", "Mazur", "Vova", "Принтер", "Ремонт", "мажет изображение, скрипит", "2022-10-02","","Ivanov","Alex"))
+        items.add(DealModel(261,"новая", "Viktuk", "Sasha", "Принтер", "Настройка", "не печатает", "2022-09-30","","",""))
+        items.add(DealModel(444,"закрыта", "Yanin", "Vova", "Принтер", "Ремонт", "не берет бумагу", "2022-11-11","2022-11-11","Ivanov","Petr"))
 
-
-//        val items = mutableListOf<DealModel>()
-//        items.add(DealModel(123,"новая", "Petrov", "Vasya", "Принтер", "Ремонт", "не включается", "2022-09-11","","",""))
-//        items.add(DealModel(135,"новая", "Arhipov", "Artem", "Компьютер", "Ремонт", "не включается", "2022-01-22","","",""))
-//        items.add(DealModel(295,"новая", "Sidirov", "Jenya", "Монитор", "Ремонт", "не включается", "2022-11-11","","",""))
-//        items.add(DealModel(335,"в работе", "Ivanov", "Ivan", "Принтер", "Ремонт", "не включается", "2022-10-02","","Komkov","Alex"))
-//        items.add(DealModel(113,"новая", "Afonin", "Vasya", "Монитор", "Ремонт", "не включается", "2022-04-05","","",""))
-//        items.add(DealModel(266,"новая", "Suvorov", "Petr", "Принтер", "Настройка", "не печатает", "2022-09-30","","",""))
-//        items.add(DealModel(359,"закрыта", "Suvorov", "Vova", "Компьютер", "Установка ОС", "не загружается", "2022-05-10","2022-06-11","Ivanov","Petr"))
-//        items.add(DealModel(135,"в работе", "Ivanov", "Vasya", "Принтер", "Картридж", "кончилась краска", "2022-10-02","","Komkov","Alex"))
-//        items.add(DealModel(291,"в работе", "Lenin", "Yan", "Компьютер", "Вирусы", "заблокирован", "2022-10-02","","Ivanov","Alex"))
-//        items.add(DealModel(385,"в работе", "Newton", "Isak", "Монитор", "Настройка", "изображение розовое", "2022-10-02","","Ivanov","Alex"))
-//        items.add(DealModel(523,"закрыта", "Petrov", "Vasya", "Принтер", "Ремонт", "не печтатает", "2022-11-13","2022-11-16","Komkov","Alex"))
-//        items.add(DealModel(216,"новая", "Isaev", "Kirill", "Компьютер", "Настройка", "нет второго диска", "2022-09-30","","",""))
-//        items.add(DealModel(381,"в работе", "Mazur", "Vova", "Принтер", "Ремонт", "мажет изображение, скрипит", "2022-10-02","","Ivanov","Alex"))
-//        items.add(DealModel(261,"новая", "Viktuk", "Sasha", "Принтер", "Настройка", "не печатает", "2022-09-30","","",""))
-//        items.add(DealModel(444,"закрыта", "Yanin", "Vova", "Принтер", "Ремонт", "не берет бумагу", "2022-11-11","2022-11-11","Ivanov","Petr"))
-
-
-
-//        binding.rvDeal.layoutManager     = LinearLayoutManager(this)
-//        val items : MutableList<DealModel> = dealList()
-//        recyclerView = binding.rvDeal
-//        recyclerView.adapter           = DealAdapter(
-            //Список элементов
-//            items,
+        /** работа с RecyclerView на макете(виде) frag_job_list_navigator*/
+        recyclerView = binding.rvDeal
+        recyclerView.adapter           = DealAdapter(
+//            Список элементов
+            items,
 //            //Обработчик клика по элементу.
 //            { index, item ->
 //                //Вызов активности с информацией по объекту, передача туда параметров.
@@ -88,7 +70,7 @@ class CJobListNavigator : Fragment() //, View.OnClickListener
 //                items.removeAt(index)
 //                binding.rvDeal.adapter?.notifyItemRemoved(index)
 //            }
-//        )
+        )
     }
 
     /************************************************************************************************
@@ -119,66 +101,5 @@ class CJobListNavigator : Fragment() //, View.OnClickListener
 //        }
 //    }
 
-    fun dealList()                       : ArrayList<DealModel> {
-    //Тестовый список объектов, которые будут выводится пользователю.
-        val items                       = mutableListOf<DealModel>()
-        items.add(DealModel(123,"новая", "Petrov", "Vasya", "Принтер", "Ремонт", "не включается", "2022-09-11","","",""))
-        items.add(DealModel(135,"новая", "Arhipov", "Artem", "Компьютер", "Ремонт", "не включается", "2022-01-22","","",""))
-        items.add(DealModel(295,"новая", "Sidirov", "Jenya", "Монитор", "Ремонт", "не включается", "2022-11-11","","",""))
-        items.add(DealModel(335,"в работе", "Ivanov", "Ivan", "Принтер", "Ремонт", "не включается", "2022-10-02","","Komkov","Alex"))
-        items.add(DealModel(113,"новая", "Afonin", "Vasya", "Монитор", "Ремонт", "не включается", "2022-04-05","","",""))
-        items.add(DealModel(266,"новая", "Suvorov", "Petr", "Принтер", "Настройка", "не печатает", "2022-09-30","","",""))
-        items.add(DealModel(359,"закрыта", "Suvorov", "Vova", "Компьютер", "Установка ОС", "не загружается", "2022-05-10","2022-06-11","Ivanov","Petr"))
-        items.add(DealModel(135,"в работе", "Ivanov", "Vasya", "Принтер", "Картридж", "кончилась краска", "2022-10-02","","Komkov","Alex"))
-        items.add(DealModel(291,"в работе", "Lenin", "Yan", "Компьютер", "Вирусы", "заблокирован", "2022-10-02","","Ivanov","Alex"))
-        items.add(DealModel(385,"в работе", "Newton", "Isak", "Монитор", "Настройка", "изображение розовое", "2022-10-02","","Ivanov","Alex"))
-        items.add(DealModel(523,"закрыта", "Petrov", "Vasya", "Принтер", "Ремонт", "не печтатает", "2022-11-13","2022-11-16","Komkov","Alex"))
-        items.add(DealModel(216,"новая", "Isaev", "Kirill", "Компьютер", "Настройка", "нет второго диска", "2022-09-30","","",""))
-        items.add(DealModel(381,"в работе", "Mazur", "Vova", "Принтер", "Ремонт", "мажет изображение, скрипит", "2022-10-02","","Ivanov","Alex"))
-        items.add(DealModel(261,"новая", "Viktuk", "Sasha", "Принтер", "Настройка", "не печатает", "2022-09-30","","",""))
-        items.add(DealModel(444,"закрыта", "Yanin", "Vova", "Принтер", "Ремонт", "не берет бумагу", "2022-11-11","2022-11-11","Ivanov","Petr"))
-        return items as ArrayList<DealModel>
-    }
-    private fun initial() {
-        recyclerView = binding.rvDeal
-        adapter = DealAdapter()
-        recyclerView.adapter = adapter
-        adapter.setList(dealList())
-
-    }
-//    fun allDeal (): ArrayList<DealModel> {
-//        val userList = ArrayList<DealModel>()
-//        val  user = DealModel (133,"Petrov", "Vasya")
-//        userList.add(user)
-//        val  user2 = DealModel (321,"Ivanov", "Ilya")
-//        userList.add(user2)
-//        val  user3 = DealModel (654,"Sidirov", "Ivan")
-//        userList.add(user3)
-//        val  user4 = DealModel (987,"Arhipov", "Petr")
-//        userList.add(user4)
-//        val  user5 = DealModel (789,"Afonin", "Kolya")
-//        userList.add(user5)
-//        val  user6 = DealModel (456,"Petrov", "Vasya")
-//        userList.add(user6)
-//        val  user7 = DealModel (984,"Ivanov", "Ilya")
-//        userList.add(user7)
-//        val  user8 = DealModel (962,"Sidirov", "Ivan")
-//        userList.add(user8)
-//        val  user9 = DealModel (364,"Arhipov", "Petr")
-//        userList.add(user9)
-//        val  user10 = DealModel (256,"Afonin", "Kolya")
-//        userList.add(user10)
-//        val  user11 = DealModel (136,"Petrov", "Vasya")
-//        userList.add(user11)
-//        val  user12 = DealModel (234,"Ivanov", "Ilya")
-//        userList.add(user12)
-//        val  user13 = DealModel (192,"Sidirov", "Ivan")
-//        userList.add(user13)
-//        val  user14 = DealModel (852,"Arhipov", "Petr")
-//        userList.add(user14)
-//        val  user15 = DealModel (174,"Afonin", "Kolya")
-//        userList.add(user15)
-//        return userList
-//    }
 
 }
